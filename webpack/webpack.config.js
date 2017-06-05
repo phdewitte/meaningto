fc52f0bd7dd6326da196f6b6ce5071aa23af7546
Patrick DeWitte <pdewitte@grubhub.com>
2017-06-04 22:36:19 -0500

    Add css component support; Add eslint; Clean up webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,48 +4,57 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = env => {
-    return {
-        entry: {
-            app: path.join(__dirname, '../src'),
-            vendor: ['react', 'react-dom'],
-        },
+  return {
+    entry: {
+      app: path.join(__dirname, '../src'),
+      vendor: ['react', 'react-dom'],
+    },
 
-        output: {
-            filename: '[name].[hash].js',
-            path: path.join(__dirname, '../dist'),
-        },
+    output: {
+      filename: '[name].[hash].js',
+      path: path.join(__dirname, '../dist'),
+    },
 
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    query: {
-                        cacheDirectory: true,
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    loader: ExtractTextPlugin.extract({
-                        use: 'css-loader?importLoaders=1!postcss-loader',
-                    }),
-                },
-            ],
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            cacheDirectory: true,
+          },
         },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            { loader:
+              'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[local]',
+              },
+            },
+            'postcss-loader',
+          ],
+        },
+      ],
+    },
 
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor',
-                minChunks: Infinity,
-                filename: '[name].[hash].js',
-            }),
-            new HtmlWebpackPlugin({
-                template: path.join(__dirname, '../src/index.html'),
-                filename: 'index.html',
-                inject: 'body',
-            }),
-            new ExtractTextPlugin('[name].bundle.css'),
-        ],
-    };
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: Infinity,
+        filename: '[name].[hash].js',
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, '../src/index.html'),
+        filename: 'index.html',
+        inject: 'body',
+      }),
+    ],
+  };
 };
